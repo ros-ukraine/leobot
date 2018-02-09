434fcb932a02f193054fbad8f871a7f9d3ea68df
Max Zhaloba <25727413+MaxZhaloba@users.noreply.github.com>
2018-02-09 19:23:31 +0000

    Start the web server only when the JS module is executed directly rather than included as a library

--- leobot_web_server/scripts/web_server.js
+++ leobot_web_server/scripts/web_server.js
@@ -1,5 +1,4 @@
 #!/usr/bin/env nodejs
-
 'use strict';
 
 const fs = require('fs');
@@ -7,24 +6,31 @@ const path = require('path');
 const rosnodejs = require('rosnodejs');
 const nodeStatic = require('node-static');
 
-rosnodejs.initNode('leobot_web_server').then(() => {
-    const nh = rosnodejs.nh;
+function startWebServer(){
+    rosnodejs.initNode('leobot_web_server').then(() => {
+        const nh = rosnodejs.nh;
 
-    console.log('rosnodejs handle: ' + nh);
+        console.log('rosnodejs handle: ' + nh);
 
-    nh.getParam('~port').then((port) => {
-        const webContentFolderPath = '../web_content';
-        const webContentFolderAbsolutePath = path.join(__dirname, webContentFolderPath);
-        console.log('Starting the web server in folder ' + webContentFolderAbsolutePath);
-        // This command will fail if path is incorrect
-        const webContentFolderRealPath = fs.realpathSync(webContentFolderAbsolutePath);
+        nh.getParam('~port').then((port) => {
+            const webContentFolderPath = '../web_content';
+            const webContentFolderAbsolutePath = path.join(__dirname, webContentFolderPath);
+            console.log('Starting the web server in folder ' + webContentFolderAbsolutePath);
+            // This command will fail if path is incorrect
+            const webContentFolderRealPath = fs.realpathSync(webContentFolderAbsolutePath);
 
-        const file = new(nodeStatic.Server)(webContentFolderRealPath);
+            const file = new(nodeStatic.Server)(webContentFolderRealPath);
 
-        require('http').createServer(function (request, response) {
-            file.serve(request, response);
-        }).listen(port);
+            require('http').createServer(function (request, response) {
+                file.serve(request, response);
+            }).listen(port);
 
-        console.log('Web server started at port ' + port);
+            console.log('Web server started at port ' + port);
+        });
     });
-});
+};
+
+// If this module is executed directly rather than included as a library
+if (require.main === module) {
+    startWebServer();
+};
