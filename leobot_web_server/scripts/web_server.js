cf14e5f24b9211aff54d0988aab4841ca4c55981
Max Zhaloba <25727413+MaxZhaloba@users.noreply.github.com>
2018-02-09 19:18:13 +0000

    Read the server port parameter and start the server on specified port

--- leobot_web_server/scripts/web_server.js
+++ leobot_web_server/scripts/web_server.js
@@ -12,24 +12,19 @@ rosnodejs.initNode('leobot_web_server').then(() => {
 
     console.log('rosnodejs handle: ' + nh);
 
-    nh.getParam('port').then((p) => {
-        const port = p;
+    nh.getParam('~port').then((port) => {
+        const webContentFolderPath = '../web_content';
+        const webContentFolderAbsolutePath = path.join(__dirname, webContentFolderPath);
+        console.log('Starting the web server in folder ' + webContentFolderAbsolutePath);
+        // This command will fail if path is incorrect
+        const webContentFolderRealPath = fs.realpathSync(webContentFolderAbsolutePath);
 
-        console.log('Port parameter: ' + port);
+        const file = new(nodeStatic.Server)(webContentFolderRealPath);
+
+        require('http').createServer(function (request, response) {
+            file.serve(request, response);
+        }).listen(port);
+
+        console.log('Web server started at port ' + port);
     });
 });
-
-const port = 8080; // hard-coded for now
-const webContentFolderPath = '../web_content';
-const webContentFolderAbsolutePath = path.join(__dirname, webContentFolderPath);
-console.log('Starting the web server in folder ' + webContentFolderAbsolutePath);
-// This command will fail if path is incorrect
-const webContentFolderRealPath = fs.realpathSync(webContentFolderAbsolutePath);
-
-const file = new(nodeStatic.Server)(webContentFolderRealPath);
-
-require('http').createServer(function (request, response) {
-    file.serve(request, response);
-}).listen(port);
-
-console.log('Web server started at port ' + port);
