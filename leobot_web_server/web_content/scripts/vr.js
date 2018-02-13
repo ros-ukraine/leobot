fa215fdd538abf1314939cd358da5ec5bf4285a9
SystemDiagnosticss <zdarovuyigora@gmail.com>
2018-02-13 11:04:51 +0000

    hardcoded port 8090, change variable name

--- leobot_web_server/web_content/scripts/vr.js
+++ leobot_web_server/web_content/scripts/vr.js
@@ -1,6 +1,5 @@
-var a = window.innerHeight;
-var b = window.innerWidth;
-var c = Math.round(b/2);
-console.log(a,b,c);
-document.getElementById('right_iframe').src = "http://0.0.0.0:8090/stream?topic=/leobot/stereocamera/right/image_raw&width="+c+"&height="+a;
-document.getElementById('left_iframe').src = "http://0.0.0.0:8090/stream?topic=/leobot/stereocamera/left/image_raw&width="+c+"&height="+a;
+var frameHeight = window.innerHeight;
+var frameWidth = window.innerWidth;
+var frameWidthHalf = Math.round(frameWidth/2);
+document.getElementById('right_iframe').src = "http://0.0.0.0:8090/stream?topic=/leobot/stereocamera/right/image_raw&width="+frameWidthHalf+"&height="+frameHeight;
+document.getElementById('left_iframe').src = "http://0.0.0.0:8090/stream?topic=/leobot/stereocamera/left/image_raw&width="+frameWidthHalf+"&height="+frameHeight;
