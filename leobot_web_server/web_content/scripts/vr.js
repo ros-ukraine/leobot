cdc8b6ae47333a16d67504efe83e235b2378a9dc
SystemDiagnosticss <zdarovuyigora@gmail.com>
2018-02-20 14:37:19 +0000

    set timeout

--- leobot_web_server/web_content/scripts/vr.js
+++ leobot_web_server/web_content/scripts/vr.js
@@ -1,27 +1,22 @@
-var frameHeight = window.innerHeight;
-var frameWidth = window.innerWidth;
-var frameWidthHalf = Math.round(frameWidth/2);
-document.getElementById('right_iframe').src = "http://192.168.1.107:8090/stream?topic=/leobot/stereocamera/right/image_raw&width="+frameWidthHalf+"&height="+frameHeight;
-document.getElementById('left_iframe').src = "http://192.168.1.107:8090/stream?topic=/leobot/stereocamera/left/image_raw&width="+frameWidthHalf+"&height="+frameHeight;
+var alpha, valpha, z;
+      var beta, vbeta, x;
+      var gamma, vgamma, y;
+
 
 // setup event handler to capture the orientation event and store the most recent data in a variable
-var d = document.getElementById("elem");
-d.innerHTML = 0 + " " + 1;
-var z;
-var y;
-var x;
-function handleOrientation(event) {
-    console.log("hello");
-    x = event.beta;  // In degree in the range [-180,180]
-    y = event.gamma; // In degree in the range [-90,90]
-    z = event.alpha;
-    var absolute = event.absolute;
-    console.log(x,y,z);
+
+      if (window.DeviceOrientationEvent) {
+        // Listen for the deviceorientation event and handle the raw data
+        window.addEventListener('deviceorientation', function(eventData) {
 
 
-}
+          // alpha is the compass direction the device is facing in degrees
+          alpha = eventData.alpha
 
-window.addEventListener('deviceorientation', handleOrientation);
+          }, false);
+        };
+
+
 
 // Connecting to ROS
 
@@ -30,25 +25,6 @@ var ros = new ROSLIB.Ros({
 });
 ros.on('connection', function() {
     console.log('Connected to websocket server.');
-
-
-
-
-
-    var headControl = new ROSLIB.Topic({
-        ros : ros,
-        name : '/leobot/head_position_controller/command',
-        messageType : 'std_msgs/Float64'
-    });
-
-    var alpha_radian = ((z + 360) / 360 * 2 * Math.PI) % (2 * Math.PI);
-    console.log(alpha_radian);
-    d.innerHTML = x + ", " + y + ", " + z + ", " + alpha_radian;
-    var positionMess = new ROSLIB.Message({
-        data : alpha_radian
-    });
-
-    headControl.publish(positionMess);
 });
 ros.on('error', function(error) {
     console.log('Error connecting to websocket server: ', error);
@@ -56,3 +32,9 @@ ros.on('error', function(error) {
 ros.on('close', function() {
     console.log('Connection to websocket server closed.');
 });
+
+var headControl = new ROSLIB.Topic({
+        ros : ros,
+        name : '/leobot/head_position_controller/command',
+        messageType : 'std_msgs/Float64'
+    });
