c26d3ceca7b8c86a336c0ed91750ea026dfa85f6
mzhaloba <25727413+MaxZhaloba@users.noreply.github.com>
2018-03-03 19:33:13 +0000

    Start rosbridge and attempt to connect to it using roslib
    For task: Add teleoperation from web page #51

---
+++ leobot_web_server/web_content/scripts/teleop.js
@@ -0,0 +1,13 @@
+// Connection to ROS
+var ros = new ROSLIB.Ros({
+    url : 'ws://' + window.location.hostname + ':9090'
+});
+ros.on('connection', function() {
+    console.log('Connected to websocket server.');
+});
+ros.on('error', function(error) {
+    console.log('Error connecting to websocket server: ', error);
+});
+ros.on('close', function() {
+    console.log('Connection to websocket server closed.');
+});
