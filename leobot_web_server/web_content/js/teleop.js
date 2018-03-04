87523e726ee616935de5431c18c31dd73e212f0c
Max Zhaloba <25727413+MaxZhaloba@users.noreply.github.com>
2018-03-04 19:21:07 +0000

    Implement simple forward moving command in web browser
    For task: Add teleoperation from web page #51

--- leobot_web_server/web_content/js/teleop.js
+++ leobot_web_server/web_content/js/teleop.js
@@ -1,3 +1,5 @@
+var topicName = '/leobot/wheel_diff_drive_controller/cmd_vel';
+
 // Connection to ROS
 var ros = new ROSLIB.Ros({
     url : 'ws://' + window.location.hostname + ':9090'
@@ -10,4 +12,31 @@ ros.on('error', function(error) {
 });
 ros.on('close', function() {
     console.log('Connection to websocket server closed.');
+});
+
+var topic = new ROSLIB.Topic({
+    ros : ros,
+    name : topicName,
+    messageType : 'geometry_msgs/Twist'
+});
+
+topic.subscribe(function(message) {
+    console.log('Received message on ' + topic.name + ': ', message);
+});
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    var buttons = document.getElementsByClassName('button-forward');
+
+    for (var i=0; i<buttons.length; i++) {
+        buttons[i].onclick = function() {
+            var positionMessage = new ROSLIB.Message({
+                linear: {
+                    x: 0.5,
+                    y: 0,
+                    z: 0
+                }
+            });
+            topic.publish(positionMessage);
+        };
+    }
 });
