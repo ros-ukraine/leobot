74713af89bb0d5696c880044e00ae771aa8e8115
Max Zhaloba <25727413+MaxZhaloba@users.noreply.github.com>
2018-03-04 16:41:27 +0000

    Print an error in browser console log if connection to rosbridge fails
    For task: Add teleoperation from web page #51

--- leobot_web_server/web_content/scripts/teleop.js
+++ leobot_web_server/web_content/scripts/teleop.js
@@ -6,7 +6,7 @@ ros.on('connection', function() {
     console.log('Connected to websocket server.');
 });
 ros.on('error', function(error) {
-    console.log('Error connecting to websocket server: ', error);
+    console.error('Error connecting to websocket server: ', error);
 });
 ros.on('close', function() {
     console.log('Connection to websocket server closed.');
