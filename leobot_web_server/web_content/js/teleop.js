5f6e25f5f8b0362c276f9e0c2d515a1094460d6f
Max Zhaloba <25727413+MaxZhaloba@users.noreply.github.com>
2018-03-10 10:36:08 +0000

    Implement teleoperation using arrow keys in web browser

--- leobot_web_server/web_content/js/teleop.js
+++ leobot_web_server/web_content/js/teleop.js
@@ -25,24 +25,49 @@ topic.subscribe(function(message) {
 });
 
 document.addEventListener('DOMContentLoaded', function(event) {
+    function publishMessage(message) {
+        var positionMessage = new ROSLIB.Message(message);
+        topic.publish(positionMessage);
+    }
+
     function publishMessageOnClick(elementsClass, message) {
         var elements = document.getElementsByClassName(elementsClass);
 
         for (var i=0; i<elements.length; i++) {
             elements[i].onclick = function() {
-                var positionMessage = new ROSLIB.Message(message);
-                topic.publish(positionMessage);
+                publishMessage(message);
             };
         }
     }
 
-    var forwardMessage  = {  linear: { x:  1, y:  0, z:   0 } };
-    var leftMessage     = { angular: { x:  0, y:  0, z: -15 } };
-    var rightMessage    = { angular: { x:  0, y:  0, z:  15 } };
-    var backwardMessage = {  linear: { x: -1, y:  0, z:   0 } };
+    var forwardMessage  = {  linear: { x:  1, y: 0, z:   0 } };
+    var leftMessage     = { angular: { x:  0, y: 0, z: -15 } };
+    var rightMessage    = { angular: { x:  0, y: 0, z:  15 } };
+    var backwardMessage = {  linear: { x: -1, y: 0, z:   0 } };
 
     publishMessageOnClick('button-forward',  forwardMessage);
     publishMessageOnClick('button-left',     leftMessage);
     publishMessageOnClick('button-right',    rightMessage);
     publishMessageOnClick('button-backward', backwardMessage);
+
+    window.addEventListener('keydown', function(e) {
+        switch(e.key){
+            case 'ArrowUp':
+                publishMessage(forwardMessage);
+                e.preventDefault();
+                break;
+            case 'ArrowLeft':
+                publishMessage(leftMessage);
+                e.preventDefault();
+                break;
+            case 'ArrowRight':
+                publishMessage(rightMessage);
+                e.preventDefault();
+                break;
+            case 'ArrowDown':
+                publishMessage(backwardMessage);
+                e.preventDefault();
+                break;
+        }
+    });
 });
