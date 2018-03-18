06fedb346fb5e469215ff2853f1b8388c2e7c11f
Max Zhaloba <25727413+MaxZhaloba@users.noreply.github.com>
2018-03-18 22:51:27 +0000

    Refactor the message publishing

--- leobot_web_server/web_content/js/main.js
+++ leobot_web_server/web_content/js/main.js
@@ -1,3 +1,5 @@
+var headDelta = 0;
+
 // Connection to ROS
 var ros = new ROSLIB.Ros({
     url : 'ws://' + window.location.hostname + ':9090'
@@ -12,88 +14,80 @@ ros.on('close', function() {
     console.log('Connection to websocket server closed.');
 });
 
+// todo: create function
 var wheelsTopic = new ROSLIB.Topic({
-    ros : ros,
-    name : '/leobot/wheel_diff_drive_controller/cmd_vel',
+    ros: ros,
+    name: '/leobot/wheel_diff_drive_controller/cmd_vel',
     messageType : 'geometry_msgs/Twist'
 });
-
 var headControlTopic = new ROSLIB.Topic({
-    ros : ros,
+    ros: ros,
     name : '/leobot/head_position_controller/command',
     messageType : 'std_msgs/Float64'
 });
 var headListenerTopic = new ROSLIB.Topic({
-    ros : ros,
-    name : '/leobot/head_position_controller/state',
+    ros: ros,
+    name: '/leobot/head_position_controller/state',
     messageType : 'control_msgs/JointControllerState'
 });
 
 wheelsTopic.subscribe(function(message) {
     console.debug('Received message on ' + wheelsTopic.name + ': ', message);
 });
-
 headListenerTopic.subscribe(function(message) {
     headDelta = message.process_value * 180 / Math.PI;
 });
 
-document.addEventListener('DOMContentLoaded', function(event) {
-    var headDelta = 0;
+function publishMessage(message, topic) {
+    topic.publish(new ROSLIB.Message(message));
+}
 
+function publishMessageOnClick(elementsClass, message, topic) {
+    var elements = document.getElementsByClassName(elementsClass);
+
+    for (var i=0; i<elements.length; i++) {
+        elements[i].onclick = function() {
+            publishMessage(message, topic);
+        };
+    }
+}
+
+function publishHeadPosition() {
+    var delta_radians =  headDelta / 180.0 * Math.PI;
+    console.log('degrees = ' + headDelta + ' radians = ' + delta_radians);
+    publishMessage({data: delta_radians}, headControlTopic);
+}
+
+document.addEventListener('DOMContentLoaded', function(event) {
     var siteRoot = window.location.protocol + '//' + window.location.hostname + ':8090' + '/';
     document.getElementsByClassName('video-streaming')[0].src = siteRoot + 'stream?topic=/leobot/stereocamera/left/image_raw&width=640&height=470';
-
-    function publishHeadPosition() {
-        var delta_radians =  headDelta / 180.0 * Math.PI;
-        console.log('degrees = ' + headDelta + ' radians = ' + delta_radians);
-        var positionMess = new ROSLIB.Message({
-            data : delta_radians
-        });
-        headControlTopic.publish(positionMess);
-    }
-
-    function publishMessage(message) {
-        var positionMessage = new ROSLIB.Message(message);
-        // todo: pass as parameter
-        wheelsTopic.publish(positionMessage);
-    }
-
-    function publishMessageOnClick(elementsClass, message) {
-        var elements = document.getElementsByClassName(elementsClass);
-
-        for (var i=0; i<elements.length; i++) {
-            elements[i].onclick = function() {
-                publishMessage(message);
-            };
-        }
-    }
 
     var forwardMessage  = {  linear: { x:  1, y: 0, z:   0 } };
     var leftMessage     = { angular: { x:  0, y: 0, z: -15 } };
     var rightMessage    = { angular: { x:  0, y: 0, z:  15 } };
     var backwardMessage = {  linear: { x: -1, y: 0, z:   0 } };
 
-    publishMessageOnClick('button-forward',  forwardMessage);
-    publishMessageOnClick('button-left',     leftMessage);
-    publishMessageOnClick('button-right',    rightMessage);
-    publishMessageOnClick('button-backward', backwardMessage);
+    publishMessageOnClick('button-forward',  forwardMessage,  wheelsTopic);
+    publishMessageOnClick('button-left',     leftMessage,     wheelsTopic);
+    publishMessageOnClick('button-right',    rightMessage,    wheelsTopic);
+    publishMessageOnClick('button-backward', backwardMessage, wheelsTopic);
 
     window.addEventListener('keydown', function(e) {
         switch(e.key){
             case 'ArrowUp':
-                publishMessage(forwardMessage);
+                publishMessage(forwardMessage, wheelsTopic);
                 e.preventDefault();
                 break;
             case 'ArrowLeft':
-                publishMessage(leftMessage);
+                publishMessage(leftMessage, wheelsTopic);
                 e.preventDefault();
                 break;
             case 'ArrowRight':
-                publishMessage(rightMessage);
+                publishMessage(rightMessage, wheelsTopic);
                 e.preventDefault();
                 break;
             case 'ArrowDown':
-                publishMessage(backwardMessage);
+                publishMessage(backwardMessage, wheelsTopic);
                 e.preventDefault();
                 break;
         }
