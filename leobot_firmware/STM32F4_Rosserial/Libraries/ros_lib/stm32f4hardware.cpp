08db6416c46163c2e5ea0ebe025958981adf21e1
vasyl_yosypenko <v.v.yosypenko@gmail.com>
2018-04-30 16:30:29 +0300

    renamed firwmare folder

---
+++ leobot_firmware/STM32F4_Rosserial/Libraries/ros_lib/stm32f4hardware.cpp
@@ -0,0 +1,45 @@
+/*
+ * STM32Hardware.cpp
+ *
+ *  Created on: 29 квіт. 2018 р.
+ *      Author: Nemo
+ */
+
+
+#include "stm32f4hardware.h"
+
+class STM32F4Hardware;
+
+STM32F4Hardware::STM32F4Hardware()
+{
+
+}
+
+void STM32F4Hardware::init()
+{
+	//wdt_disable();
+	//avr_time_init();
+	//avr_uart_init();
+	//sei();
+}
+
+// Read a byte of data from ROS connection.
+    // If no data, returns -1
+int STM32F4Hardware::read()
+{
+	//return avr_uart_receive_byte();
+	return -1;
+}
+
+
+void STM32F4Hardware::write(uint8_t* data, int length)
+{
+
+}
+
+unsigned long STM32F4Hardware::time()
+{
+	return 1000;
+}
+
+
