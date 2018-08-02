5bb3c79803555d52459295efc2256f9f0756c754
Max Zhaloba <25727413+MaxZhaloba@users.noreply.github.com>
2018-08-02 17:32:03 +0000

    Implement conversion of ROS images to OpenCV and back

--- leobot_vision/src/leobot_vision.py
+++ leobot_vision/src/leobot_vision.py
@@ -4,6 +4,7 @@ import roslib
 roslib.load_manifest('leobot_vision')
 import sys
 import rospy
+import cv2
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 
@@ -11,28 +12,26 @@ class image_converter:
     def __init__(self):
         self.image_pub = rospy.Publisher("/camera", Image, queue_size=2)
 
-        # self.bridge = CvBridge()
+        self.bridge = CvBridge()
         self.image_sub = rospy.Subscriber("/right_fisheye_camera/image_raw", Image, self.callback)
 
     def callback(self, data):
         try:
-            # cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
-            print 'Callback!'
-            self.image_pub.publish(data)
+            cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
         except CvBridgeError as e:
             print(e)
 
-        # (rows, cols, channels) = cv_image.shape
-        # if cols > 60 and rows > 60:
-        #     cv2.circle(cv_image, (50, 50), 10, 255)
-        #
-        # cv2.imshow("Image window", cv_image)
-        # cv2.waitKey(3)
-        #
-        # try:
-        #     self.image_pub.publish(self.bridge.cv2_to_imgmsg(cv_image, "bgr8"))
-        # except CvBridgeError as e:
-        #     print(e)
+        (rows, cols, channels) = cv_image.shape
+        if cols > 60 and rows > 60:
+            cv2.circle(cv_image, (cols/2, rows/2), min(cols, rows)/2-1, (255, 255, 255))
+
+        cv2.imshow("Image window", cv_image)
+        cv2.waitKey(3)
+
+        try:
+            self.image_pub.publish(self.bridge.cv2_to_imgmsg(cv_image, "bgr8"))
+        except CvBridgeError as e:
+            print(e)
 
 
 def main(args):
@@ -42,7 +41,7 @@ def main(args):
         rospy.spin()
     except KeyboardInterrupt:
         print("Shutting down")
-    # cv2.destroyAllWindows()
+    cv2.destroyAllWindows()
 
 
 if __name__ == '__main__':
