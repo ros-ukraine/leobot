707b45d8eefdda4d1c29a4e65cf7daf2d7f5e654
Andriy Petlovanyy <andriy@noemail.com>
2019-03-13 21:39:57 +0000

    Development commit. Added ability to load hardware controllers. Velocity controllers for hardware testing. Fixed control loop timing.

--- leobot_hardware/src/hardware_node.cpp
+++ leobot_hardware/src/hardware_node.cpp
@@ -7,14 +7,17 @@ using leobot_hardware::LeobotRobotHW;
 
 int main(int argc, char **argv)
 {
+  const double ACTUAL_CYCLE = 0.75;  
   std::string node_name = "leobot_hardware_node";
 
   ros::init(argc, argv, node_name);
+  ros::NodeHandle top_level_node_handle;
   ros::NodeHandle root_node_handle("~");
 
   ros::NodeHandle private_node("~");
 
   ros::CallbackQueue generic_queue;
+  top_level_node_handle.setCallbackQueue(&generic_queue);
   root_node_handle.setCallbackQueue(&generic_queue);
 
   ros::AsyncSpinner spinner(2, &generic_queue);
@@ -25,18 +28,19 @@ int main(int argc, char **argv)
 
   LeobotRobotHW robot_hardware;
   robot_hardware.init(root_node_handle, private_node);
-  controller_manager::ControllerManager controller_manager(&robot_hardware, root_node_handle);
+  controller_manager::ControllerManager controller_manager(&robot_hardware, top_level_node_handle);
 
   struct timespec last_time;
   struct timespec current_time;
   clock_gettime(CLOCK_MONOTONIC, &last_time);
 
-  int loop_rate;
-  private_node.param<int>("loop_rate", loop_rate, 10);
+  double loop_rate;
+  private_node.param<double>("loop_rate", loop_rate, 10.0);
 
-  ros::Duration desired_update_period = ros::Duration(1.0 / loop_rate);
+  ros::Duration desired_update_duration = ros::Duration(1.0 / loop_rate);
+  ros::Duration communicaton_wait_duration = ros::Duration(ACTUAL_CYCLE / loop_rate);
 
-  ros::Rate rate(loop_rate);
+  ros::Rate rate(loop_rate / ACTUAL_CYCLE);
   while (ros::ok())
   {
     clock_gettime(CLOCK_MONOTONIC, &current_time);
@@ -44,16 +48,16 @@ int main(int argc, char **argv)
                                                (current_time.tv_nsec - last_time.tv_nsec) / 1e+9);
     last_time = current_time;
 
-    if (elapsed_time > desired_update_period)
+    if (elapsed_time > desired_update_duration)
     {
       ROS_WARN_STREAM_THROTTLE(2.0, "Cycle time exceeded error threshold by: " <<
-        (elapsed_time - desired_update_period) << ", cycle time: " << elapsed_time <<
-        ", desired_update_period: " << desired_update_period);
+        (elapsed_time - desired_update_duration) << ", cycle time: " << elapsed_time <<
+        ", desired_update_duration: " << desired_update_duration);
     }
 
     ros::Time timestamp = ros::Time::now();
     read_state_queue.clear();
-    read_state_queue.callOne(ros::WallDuration(desired_update_period.toSec()));
+    read_state_queue.callOne(ros::WallDuration(communicaton_wait_duration.toSec()));
     robot_hardware.read(timestamp, elapsed_time);
     controller_manager.update(ros::Time::now(), elapsed_time);
     robot_hardware.write(timestamp, elapsed_time);
