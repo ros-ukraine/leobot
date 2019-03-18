941539a24427d84e0f6c3106bb5b25f7127eb4bf
Max Zhaloba <25727413+MaxZhaloba@users.noreply.github.com>
2019-03-18 22:15:15 +0000

    Remove the unnecessary variable in JS file
    For task: Integrate Bluetooth Joystick to mobile VR set #86

--- leobot_web_server/web_content/js/config.js
+++ leobot_web_server/web_content/js/config.js
@@ -389,8 +389,7 @@ function capitalizeString(s) {
 }
 
 function getColorGradientValue(color1, color2, weight) {
-    var p = weight;
-    var w = p * 2 - 1;
+    var w = weight * 2 - 1;
     var w1 = (w/1+1) / 2;
     var w2 = 1 - w1;
     var rgb = [Math.round(color1[0] * w1 + color2[0] * w2),
