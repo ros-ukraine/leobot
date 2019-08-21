c61d105d89481478ed22136ba8d4562a1cdda8ff
Andriy Petlovanyy <AndriyPt@users.noreply.github.com>
2019-08-21 15:25:59 +0300

    Added clean up for safe restart of Virtual Desktop

--- docker/ide/web/startup.sh
+++ docker/ide/web/startup.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# Making sure that there is no left over lock for virtual desktop
+sudo rm -f /tmp/.X1-lock
 nohup Xvfb :1 -screen 0 1024x768x16 &> /home/user/gzweb/xvfb.log &
 
 pushd /home/user/c9sdk
